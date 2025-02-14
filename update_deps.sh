1145fec066844f828f91bced4033bcee552d0bb8
Zhehao 0xFF <zhehao0827@163.com>
2025-02-14 13:09:00 +0800

    chore: update deps api to latest version

---
+++ update_deps.sh
@@ -0,0 +1,7 @@
+moon update
+moon remove KCN-judu/linear-algebra
+moon remove KCN-judu/luna-utils
+moon remove Kaida-Amethyst/math
+moon add KCN-judu/linear-algebra
+moon add KCN-judu/luna-utils
+moon add Kaida-Amethyst/math
