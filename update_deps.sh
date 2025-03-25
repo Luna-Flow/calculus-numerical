2840a9fafaa9f1294145d98d5e77cff265021e14
Zhehao 0xFF <zhehao0827@163.com>
2025-03-25 15:15:45 +0800

    refactor: use fn alias to simplify implementation

--- update_deps.sh
+++ update_deps.sh
@@ -1,3 +1,11 @@
-moon update
 rm -rf .mooncakes.io
+moon update
+moon remove Luna-Flow/linear-algebra
+moon remove Luna-Flow/luna-utils
+moon remove Luna-Flow/luna-generic
+moon remove Kaida-Amethyst/math
+moon add Kaida-Amethyst/math
+moon add Luna-Flow/luna-generic
+moon add Luna-Flow/linear-algebra
+moon add Luna-Flow/luna-utils
 moon install
